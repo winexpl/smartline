5c0c790fe51f2c0cb60ca6191ecdaa062a5ca034
Maria Drevina <drevina2003@mail.ru>
2025-09-20 01:54:41 +0400

    init

---
+++ src/app/common/smart-line/smart-line.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-line',
+  imports: [],
+  templateUrl: './smart-line.component.html',
+  styleUrl: './smart-line.component.scss'
+})
+export class SmartLineComponent {
+  public searchString = signal("")
+
+  
+}
